270b71087ededdd2e50ef09bfdbcc4ec688056d3
georgianaoniceanu <georgiana939@yahoo.com>
2025-12-28 16:43:03 +0200

    Am modificat index.js sa arate linkurile in terminal

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+//importam utilitarele pt documentatia API
 const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
+//incarcam fisierul de configurare Swagger din format YAML
 const swaggerDocument = YAML.load('./swagger.yaml')
 require('dotenv').config() //incarca variabilele din .env
 
 const app = express()
 app.use(cors()) //permite frontedului sa acceseze serverul
 app.use(express.json()) //permite serverului sa citeasca date JSON
+//setam ruta unde va fi afisata documentatia interactiva
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
@@ -15,5 +18,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, ()=>{
-    console.log(`Serverul ruleaza pe portul ${PORT}`)
+    console.log(`Serverul ruleaza pe portul http://localhost:${PORT}`)
+    console.log(`Documentatie disponibila la: http://localhost:${PORT}/api-docs`)
 })
+//documentatia e vizibila la http://localhost:5000/api-docs
