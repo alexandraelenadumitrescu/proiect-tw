a96b6fbf08ad4c77a795a56dd32fbd9ea272585e
bianca <baiasbianca.bb.bb@gmail.com>
2026-01-10 13:40:18 +0200

    chore: test

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,11 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
-function App(){
-  return(
+function App() {
+  return (
     <Router> {/* Aici e zona de JSX (permite cod HTML direct in javascript)*/}
       {/* Routerul = BrowserRouter = monitorizeaza URL-ul din bara de adrese a browserului*/}
       <Routes> {/* Routes = un selector; se uita la URL si decide ce componenta trebuie afisata*/}
-        <Route path="/" element={<h1>Home Page</h1>}></Route> {/* Route = legatura dintre o cale si elementul vizual */}
+        <Route path="/" element={<h1>Test</h1>}></Route> {/* Route = legatura dintre o cale si elementul vizual */}
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
     </Router>
