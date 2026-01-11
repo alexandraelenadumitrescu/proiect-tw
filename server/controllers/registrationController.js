b07a7b95e896c27017ce0f79f04b5e93ed5c2c47
bianca <baiasbianca.bb.bb@gmail.com>
2026-01-11 13:49:47 +0200

    feat: registered users now also gain the author role by default

--- server/controllers/registrationController.js
+++ server/controllers/registrationController.js
@@ -1,6 +1,9 @@
 const prisma = require('../config/shared.js');
 const bcrypt = require('bcrypt');
 
+const roles = require('../constants/roles.js');
+const DEFAULT_USER_ROLE = roles.DEFAULT_USER_ROLE;
+
 exports.registerUser = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -8,9 +11,19 @@ exports.registerUser = async (req, res) => {
   }
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await prisma.users.create({
       data: { email, password_hash: hashedPassword },
     });
+
+    // also add a user role entry with the default role
+    const _roleEntry = await prisma.user_roles.create({
+      data: {
+        user_id: user.id,
+        role_type: DEFAULT_USER_ROLE,
+      },
+    });
+
     res.status(201).json(user);
   } catch (error) {
     console.error('Error registering user:', error);
